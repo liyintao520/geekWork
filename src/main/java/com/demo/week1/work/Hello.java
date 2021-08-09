5ae707b5785c3904c66d47779e6ddfcb7df342be
liyintao <lyt890916wlj@sina.com>
2021-08-09 12:09:51 +0800

    选做题1

--- src/main/java/com/demo/week1/work/Hello.java
+++ src/main/java/com/demo/week1/work/Hello.java
@@ -10,15 +10,17 @@ package com.demo.week1.work;
 public class Hello {
     public static void main(String[] args) {
         int num1 = 1;
-        double num2 = 2.0D;
-        long num3 = 3L;
-        byte num4 = 4;
+        double num2 = 1.0D;
+        long num3 = 1L;
+        byte num4 = 1;
         if (true) {
-            // 错误用法: num2 + num3 = 2.03
-            System.out.println("错误用法: num2 + num3 = " + num2 + num3);
+            // 错误用法: num2 + num3 = 1.01 不同类型操作，默认强制转换成
+            Object obj = num2 + num3;
+            System.out.println("错误用法: num2 + num3 = " + obj);
+            System.out.println("结果类型为：" + obj.getClass().getCanonicalName());
         }
         for (int i = 0; i < num1; i++) {
-            // 四则运算: num1 * num4 = 4
+            // 四则运算: num1 * num4 = 1
             System.out.println("四则运算: num1 * num4 = " + num1 * num4);
         }
     }
